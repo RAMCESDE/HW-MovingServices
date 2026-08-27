2190264a72fb9ebc1136ba9479bba0225cd9a4ac
Didier Diez Loaiza <didier10@MacBook-Neo-de-Didier.local>
2026-08-26 22:36:44 -0500

    se disena clase reserva

--- src/main/java/app/domain/Reserva.java
+++ src/main/java/app/domain/Reserva.java
@@ -1,4 +1,54 @@
 package app.domain;
 
 public class Reserva {
+    private String idReserva;
+    private String fechaReserva;
+    private String fechaServicio;
+    private String estado;
+
+    public Reserva(){
+    }
+
+
+    public Reserva(String idReserva, String fechaReserva, String fechaServicio, String estado){
+        this.idReserva= idReserva;
+        this.fechaReserva= fechaReserva;
+        this.fechaServicio= fechaServicio;
+        this.estado= estado;
+    }
+
+    public String getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(String idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public String getFechaReserva() {
+        return fechaReserva;
+    }
+
+    public void setFechaReserva(String fechaReserva) {
+        this.fechaReserva = fechaReserva;
+    }
+
+    public String getFechaServicio() {
+        return fechaServicio;
+    }
+
+    public void setFechaServicio(String fechaServicio) {
+        this.fechaServicio = fechaServicio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+
+
 }
